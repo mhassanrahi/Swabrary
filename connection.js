982d3d898c67eac893cc9c0538ed0a3d40527ce2
Hassan <m.hassan.rahi@gmail.com>
2019-12-31 14:07:41 +0500

    Initial Commit

---
+++ connection.js
@@ -0,0 +1,15 @@
+if (process.env.NODE_MODULES !== 'production') {
+    require('dotenv').config()
+}
+const mongoose = require('mongoose');
+const uri = process.env.DATABASE_URL;
+const config = { useNewUrlParser: true, useUnifiedTopology: true };
+mongoose.connect(uri, config)
+
+const db = mongoose.connection
+
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('Connected to DB.'))
+
+mongoose.set('useFindAndModify', false);
+module.exports = mongoose
---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const expressLayouts = require('express-ejs-layouts')
+
+
+const indexRoutes = require('./routes/index')
+const connection = require('./connection')
+
+
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+app.set('layout', 'layouts/layout')
+app.use(expressLayouts)
+app.use(express.static('public'))
+
+
+
+
+app.get('/', indexRoutes)
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log('UP and running on port ' + port)
+})
