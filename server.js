00e9a888c954a2837f1af55a6b1f0a7e6324c153
Hassan <m.hassan.rahi@gmail.com>
2020-01-01 20:03:36 +0500

    NODE_ENV fixed

--- server.js
+++ server.js
@@ -1,24 +1,14 @@
-if (process.env.NODE_MODULES !== 'production') {
+if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
 }
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
-const mongoose = require('mongoose');
 
 
 const indexRoutes = require('./routes/index')
-    // const connection = require('./connection')
-const uri = process.env.DATABASE_URL;
-const config = { useNewUrlParser: true, useUnifiedTopology: true };
-mongoose.connect(uri, config)
+const connection = require('./connection')
 
-const db = mongoose.connection
-
-db.on('error', error => console.error(error))
-db.once('open', () => console.log('Connected to DB.'))
-
-mongoose.set('useFindAndModify', false);
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
